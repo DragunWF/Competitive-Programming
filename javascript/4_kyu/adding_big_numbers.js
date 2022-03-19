90ec99f6e494062ae210d2dbaa10a032c4c117e0
DragonWF <plarisanmarc@gmail.com>
2022-03-19 16:46:28 +0800

    Removing file for a replacement

--- javascript/4_kyu/adding_big_numbers.js
+++
@@ -1,7 +0,0 @@
-// https://www.codewars.com/kata/525f4206b73515bffb000b21/train/javascript
-
-function add(a, b) {
-  return BigInt(String(Number(a) + Number(b)));
-}
-
-console.log(add("123", "321"));
