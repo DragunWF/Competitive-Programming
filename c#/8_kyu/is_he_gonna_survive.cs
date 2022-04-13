2ed4a8a5c9564a5541778499bf5f34c2d1b6fea6
DragonWF <plarisanmarc@gmail.com>
2022-04-13 13:31:01 +0800

    Solved kata

---
+++ c#/8_kyu/is_he_gonna_survive.cs
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/csharp
+
+class Kata
+{
+    public static bool Hero(int bullets, int dragons)
+    {
+       return bullets >= dragons * 2;
+    }
+}
