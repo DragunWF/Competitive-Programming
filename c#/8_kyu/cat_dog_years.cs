57b477e08a1903a87683841a7fc557c27f99319f
DragonWF <plarisanmarc@gmail.com>
2022-04-22 20:22:55 +0800

    Solved kata

---
+++ c#/8_kyu/cat_dog_years.cs
@@ -0,0 +1,19 @@
+public class Dinglemouse {
+  public static int[] humanYearsCatYearsDogYears(int humanYears) {
+    var catYears = CalculateYears(humanYears, "cat");
+    var dogYears = CalculateYears(humanYears, "dog");
+    return new int[]{humanYears, catYears, dogYears};
+  }
+  
+  static int CalculateYears(int years, string type) {
+    var animalYears = 0;
+    for (var i = 1; i <= years; i++) {
+      if (i == 1) animalYears += 15;
+      else if (i == 2) animalYears += 9;
+      else animalYears += type == "cat" ? 4 : 5; 
+    }
+    return animalYears;
+  }
+}
+
+// https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/csharp
