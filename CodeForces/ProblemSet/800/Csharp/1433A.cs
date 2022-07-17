823fafd064d9ee4ccc5e95d3000719b770990088
dragunwf <plarisanmarc@gmail.com>
2022-07-17 17:40:40 +0800

    Optimized solution 1433A

--- CodeForces/ProblemSet/800/Csharp/1433A.cs
+++ CodeForces/ProblemSet/800/Csharp/1433A.cs
@@ -21,8 +21,8 @@ public class CodeForces
         int n = int.Parse(x.ToString()[0].ToString());
 
         int p = (n - 1) * 10;
-        for (int i = 0; i < digitCount; i++)
-            p += i + 1;
+        for (int i = 1; i <= digitCount; i++)
+            p += i;
 
         return p;
     }
