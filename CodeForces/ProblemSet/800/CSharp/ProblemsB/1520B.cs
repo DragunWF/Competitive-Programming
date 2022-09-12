86e3b454cd3b7c9aaab39b23b591597e63b19132
dragunwf <plarisanmarc@gmail.com>
2022-09-12 22:43:22 +0800

    Fixed code formatting

--- CodeForces/ProblemSet/800/CSharp/ProblemsB/1520B.cs
+++ CodeForces/ProblemSet/800/CSharp/ProblemsB/1520B.cs
@@ -5,33 +5,33 @@ using System.Linq;
 
 public sealed class CodeForces
 {
-	private static void Main()
-	{
-		int t = int.Parse(Console.ReadLine());
-		for (int i = 0; i < t; i++)
+    private static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        for (int i = 0; i < t; i++)
         {
-			int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
             Console.WriteLine(Solve(n));
         }
-	}
+    }
 
-	private static int Solve(int n)
-	{
-		if (n <= 9)
-			return n;
+    private static int Solve(int n)
+    {
+        if (n <= 9)
+            return n;
 
-		string s = n.ToString();
-		int digitCount = s.Length;
-		int lastDigit = int.Parse(s[0].ToString());
+        string s = n.ToString();
+        int digitCount = s.Length;
+        int lastDigit = int.Parse(s[0].ToString());
 
-		string nextOrdinary = "";
-		for (int i = 0; i < digitCount; i++)
-			nextOrdinary += lastDigit.ToString();
+        string nextOrdinary = "";
+        for (int i = 0; i < digitCount; i++)
+            nextOrdinary += lastDigit.ToString();
 
-		int extraOrdinaryNums = lastDigit - 1;
-		if (n >= int.Parse(nextOrdinary))
-			extraOrdinaryNums++;
+        int extraOrdinaryNums = lastDigit - 1;
+        if (n >= int.Parse(nextOrdinary))
+            extraOrdinaryNums++;
 
-		return (digitCount - 1) * 9 + extraOrdinaryNums;
-	}
+        return (digitCount - 1) * 9 + extraOrdinaryNums;
+    }
 }
