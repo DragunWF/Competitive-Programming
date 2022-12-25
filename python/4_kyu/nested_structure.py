86c73bce9669056b33b2a96f6aecab0c2e145cc7
DragunWF <plarisanmarc@gmail.com>
2022-12-25 22:34:01 +0800

    Solved kata - 4 kyu kata id: 520446778469526ec0000001

---
+++ python/4_kyu/nested_structure.py
@@ -0,0 +1,29 @@
+# https://www.codewars.com/kata/520446778469526ec0000001
+from rich import print
+
+
+def same_structure_as(original, other) -> bool:
+    if type(original) != type(other):
+        return False
+    list_A = [count_nested(x) for x in original]
+    list_B = [count_nested(x) for x in other]
+    return list_A == list_B
+
+
+def count_nested(item):
+    if type(item) == list:
+        return [count_nested(x) for x in item]
+    return True
+
+
+def test():
+    test_cases = [
+        [[1, [1, 1]], [2, [2, 2]]],
+        [[1, [1, 1]], [[2, 2], 2]]
+    ]
+    for case in test_cases:
+        print(same_structure_as(case[0], case[1]))
+
+
+if __name__ == "__main__":
+    test()
