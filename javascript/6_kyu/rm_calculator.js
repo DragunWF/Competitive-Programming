103faca5e4e816ac38887caa2158600c1664ef1d
dragunwf <plarisanmarc@gmail.com>
2023-01-28 12:32:29 +0800

    Solved kata - 6 kyu https://www.codewars.com/kata/595bbea8a930ac0b91000130

---
+++ javascript/6_kyu/rm_calculator.js
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/595bbea8a930ac0b91000130
+
+function calculate1RM(w, r) {
+  if (r === 1) return w;
+  if (!r) return 0;
+  const calculations = [
+    w * (1 + r / 30),
+    (100 * w) / (101.3 - 2.67123 * r),
+    w * r ** 0.1,
+  ];
+  let max = calculations[0];
+  for (let i = 1; i < calculations.length; i++) {
+    if (calculations[i] > max) max = calculations[i];
+  }
+  return Math.round(max);
+}
+
+// Not part of the solution
+function test() {
+  const testCases = [
+    [135, 20, 282],
+    [200, 8, 253],
+    [270, 2, 289],
+    [360, 1, 360],
+    [400, 0, 0],
+  ];
+  for (let i = 0; i < testCases.length; i++) {
+    const result =
+      calculate1RM(testCases[i][0], testCases[i][1]) === testCases[i][2];
+    console.log(`Test Case #${i + 1}: ${result}`);
+    if (!result) {
+      console.log("Failed Case:", testCases[i]);
+    }
+  }
+}
+
+test();
