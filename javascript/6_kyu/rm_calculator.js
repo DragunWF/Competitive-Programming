155ef417bf653216c328fcd14ef08d9819868f5d
dragunwf <plarisanmarc@gmail.com>
2023-01-28 12:35:05 +0800

    Update kata solution https://www.codewars.com/kata/595bbea8a930ac0b91000130

--- javascript/6_kyu/rm_calculator.js
+++ javascript/6_kyu/rm_calculator.js
@@ -3,16 +3,9 @@
 function calculate1RM(w, r) {
   if (r === 1) return w;
   if (!r) return 0;
-  const calculations = [
-    w * (1 + r / 30),
-    (100 * w) / (101.3 - 2.67123 * r),
-    w * r ** 0.1,
-  ];
-  let max = calculations[0];
-  for (let i = 1; i < calculations.length; i++) {
-    if (calculations[i] > max) max = calculations[i];
-  }
-  return Math.round(max);
+  return Math.round(
+    Math.max(w * (1 + r / 30), (100 * w) / (101.3 - 2.67123 * r), w * r ** 0.1)
+  );
 }
 
 // Not part of the solution
