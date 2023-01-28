68b3603182957dd3151c254f9a00c5e8027c3d40
dragunwf <plarisanmarc@gmail.com>
2023-01-28 11:02:26 +0800

    Solved kata - 6 kyu https://www.codewars.com/kata/53046ceefe87e4905e00072a/train/javascript

---
+++ javascript/6_kyu/palindrome_dome.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/53046ceefe87e4905e00072a/train/javascript
+
+function palindrome(string) {
+  const validChars = "abcdefghijklmnopqrstuvwxyz0123456789".split("");
+  const raw = string.split("").filter((n) => {
+    return validChars.includes(n.toLowerCase());
+  });
+  const original = convert(raw);
+  raw.reverse();
+  return original === convert(raw);
+}
+
+function convert(arr) {
+  return arr.join("").toLowerCase();
+}
+
+// Not part of the solution
+function test() {
+  const testCases = [
+    "Amore, Roma",
+    "A man, a plan, a canal: Panama",
+    "No 'x' in 'Nixon'",
+    "Abba Zabba, you're my only friend",
+  ];
+  for (let testCase of testCases) {
+    console.log(palindrome(testCase));
+  }
+}
+
+test();
