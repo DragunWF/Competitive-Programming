af5dc6c448c3c0485ce27f62591b573ea1ecf9a6
dragunwf <plarisanmarc@gmail.com>
2023-04-07 21:18:19 +0800

    Solve kata https://www.codewars.com/kata/576757b1df89ecf5bd00073b

---
+++ javascript/6_kyu/build_tower.js
@@ -0,0 +1,38 @@
+// https://www.codewars.com/kata/576757b1df89ecf5bd00073b
+
+function towerBuilder(nFloors) {
+  const output = [];
+  const rowLength = nFloors * 2 - 1;
+  let stars = 1;
+  for (let i = 0; i < nFloors; i++) {
+    const sideLength = Math.ceil(rowLength / 2) - stars + i;
+    output.push(generateFloor(rowLength, stars, sideLength));
+    stars += 2;
+  }
+  return output;
+}
+
+function generateFloor(length, mid, side) {
+  let output = "";
+  for (let i = 0; i < 2; i++) {
+    for (let j = 1; j <= side; j++) {
+      output += " ";
+    }
+    if (i === 0) {
+      for (let j = 0; j < mid; j++) {
+        output += "*";
+      }
+    }
+  }
+  return output;
+}
+
+// Not part of the solution
+function test() {
+  const testCasesEnd = 10;
+  for (let i = 6; i <= testCasesEnd; i++) {
+    console.log(towerBuilder(i));
+  }
+}
+
+test();
