cba6cea42c78988bb8ad9dc59491d228760adb5b
dragunwf <plarisanmarc@gmail.com>
2023-04-08 12:35:12 +0800

    Solve kata https://www.codewars.com/kata/5255892719453db6d2000a23

---
+++ javascript/5_kyu/government_restrictions.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5255892719453db6d2000a23
+
+var anarchy = function () {
+  const a = ["zero", "one", "two", "three"];
+  return `I can write numbers like, ${a.indexOf("one")}, ${a.indexOf(
+    "two"
+  )}, ${a.indexOf("three")}.`;
+};
+
+// Not part of the solution
+console.log(anarchy());
