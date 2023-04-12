b9b9292ca75cb71aebb183edf189eb09000f4735
Marc Plarisan <plarisanmarc@gmail.com>
2023-04-12 17:06:08 +0800

    Update rot13.js

--- javascript/5_kyu/rot13.js
+++ javascript/5_kyu/rot13.js
@@ -1,6 +1,6 @@
 // Same solution for both of these katas
 // https://codewars.com/kata/530e15517bc88ac656000716
-// https://www.codewars.com/kata/52223df9e8f98c7aa7000062
+// https://codewars.com/kata/52223df9e8f98c7aa7000062
 
 function rot13(message) {
   let output = "";
