50d76dd59d6c980ce2fd3cdaf1ef021cec4cea52
Marc Plarisan <plarisanmarc@gmail.com>
2023-06-14 15:07:08 +0800

    Refine kata solution

--- javascript/6_kyu/consonant_value.js
+++ javascript/6_kyu/consonant_value.js
@@ -9,12 +9,25 @@ function solve(s) {
 
 function getConsonantValue(str) {
   const alphabets = "abcdefghijklmnopqrstuvwxyz";
-  let value = 0;
-  for (let character of str) {
-    value += alphabets.indexOf(character) + 1;
-  }
-  return value;
+  return str.split("").reduce((a, b) => a + alphabets.indexOf(b) + 1, 0);
 }
 
 // Not part of the solution
 console.log(solve("strength"));
+
+// Previous solution
+// function solve(s) {
+//   return s
+//     .split(/[aeiou]/)
+//     .map((a) => getConsonantValue(a))
+//     .sort((a, b) => b - a)[0];
+// }
+
+// function getConsonantValue(str) {
+//   const alphabets = "abcdefghijklmnopqrstuvwxyz";
+//   let value = 0;
+//   for (let character of str) {
+//     value += alphabets.indexOf(character) + 1;
+//   }
+//   return value;
+// }
