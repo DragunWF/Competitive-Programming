1ba12deb55bf46f808fb2b1159474abe1c9867e6
Marc Plarisan <plarisanmarc@gmail.com>
2023-11-09 11:51:43 +0800

    Solve kata

--- python/6_kyu/abundant_numbers.py
+++ python/6_kyu/abundant_numbers.py
@@ -1,15 +1,14 @@
 # https://www.codewars.com/kata/57f3996fa05a235d49000574/train/python
 
 def abundant(h: int):
-    proper_divisors = []
     for i in range(h, 1, -1):
+        proper_divisors = [1]
         for j in range(2, i):
             if i % j == 0:
                 proper_divisors.append(j)
         total = sum(proper_divisors)
         if total > h:
-            return [[i], [total - h]]
-    return [[], []]
+            return [[i], [total - i]]
 
 
 print(abundant(15))
