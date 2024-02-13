f312cd4a9fb9fbf77817ab2d47ae6cf6052c82e8
dragunwf <plarisanmarc@gmail.com>
2024-02-13 23:49:15 +0800

    Solve kata https://www.codewars.com/kata/55a5befdf16499bffb00007b

---
+++ python/8_kyu/fundamentals_return.py
@@ -0,0 +1,24 @@
+# https://www.codewars.com/kata/55a5befdf16499bffb00007b/train/python
+
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def mod(a, b):
+    return a % b
+
+
+def exponent(a, b):
+    return a ** b
+
+
+def subt(a, b):
+    return a - b
