115073a83eef91e2845fd5a27005c9566fc5a9cb
dragunwf <plarisanmarc@gmail.com>
2024-02-15 19:59:35 +0800

    Solve kata https://www.codewars.com/kata/5ce9c1000bab0b001134f5af

---
+++ java/8_kyu/QuarterOfYear/Kata.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/java
+
+public class Kata {
+    public static int quarterOf(int month) {
+        return (int) Math.ceil(month / 3f);
+    }
+}
