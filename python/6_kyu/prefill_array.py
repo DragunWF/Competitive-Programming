0f9345f38cc6d8eab92f04a7b2b10a3487ed40e7
dragunwf <plarisanmarc2@gmail.com>
2024-02-18 15:32:49 +0800

    Solve kata https://www.codewars.com/kata/54129112fb7c188740000162

---
+++ python/6_kyu/prefill_array.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/54129112fb7c188740000162/train/python
+
+from string import digits
+
+
+def prefill(n, v):
+    try:
+        return [v for i in range(int(n))]
+    except (TypeError, ValueError):
+        return f"{n} is invalid"
+
+
+# (f"{v} " * n).rstrip().split(" ")
+# test code
+print(prefill(3, 1))
+print(prefill(0, 1))
