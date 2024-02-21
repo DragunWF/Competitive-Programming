14f8344ac014c34ee1a217f7d3349661b5c652b4
Marc Plarisan <plarisanmarc2@gmail.com>
2024-02-21 12:23:11 +0800

    Solve kata https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/java

---
+++ java/8_kyu/ParseNiceIntFromChar/CharProblem.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/java
+
+public class CharProblem {
+    public static int howOld(final String herOld) {
+        return herOld.charAt(0) - 48;
+    }
+}
