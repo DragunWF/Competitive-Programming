fd5c5fa74c88ee95b844b60f7549e3c400d95d44
Marc Plarisan <plarisanmarc2@gmail.com>
2024-02-23 14:09:49 +0800

    Solve kata https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/java

---
+++ java/8_kyu/GrassHopperCheckForFactor/Kata.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/java
+
+public class Kata {
+    public static boolean checkForFactor(int base, int factor) {
+        return base % factor == 0;
+    }
+}
