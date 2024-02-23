33f66499664376939980a86f5c45ec1db715ec7a
Marc Plarisan <plarisanmarc2@gmail.com>
2024-02-23 14:30:26 +0800

    Solve kata https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/python

---
+++ python/8_kyu/welcome.py
@@ -0,0 +1,24 @@
+# https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/python
+
+def greet(language) -> str:
+    db = [ ("english", "Welcome")
+        , ("czech", "Vitejte")
+        , ("danish", "Velkomst")
+        , ("dutch", "Welkom")
+        , ("estonian", "Tere tulemast")
+        , ("finnish", "Tervetuloa")
+        , ("flemish", "Welgekomen")
+        , ("french", "Bienvenue")
+        , ("german", "Willkommen")
+        , ("irish", "Failte")
+        , ("italian", "Benvenuto")
+        , ("latvian", "Gaidits")
+        , ("lithuanian", "Laukiamas")
+        , ("polish", "Witamy")
+        , ("spanish", "Bienvenido")
+        , ("swedish", "Valkommen")
+        , ("welsh", "Croeso") ]
+    for data in db:
+        if data[0] == language:
+            return data[1]
+    return "Welcome"
