e70cd79638e88567ba71bf118c14fd18198c6e11
dragunwf <plarisanmarc2@gmail.com>
2024-02-27 17:33:47 +0800

    Solve kata https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/java

---
+++ java/8_kyu/GrasshopperSayHello/GrassHopper.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/java
+
+public class GrassHopper {
+    public static String sayHello(String name) {
+        return String.format("Hello, %s", name);
+    }
+}
