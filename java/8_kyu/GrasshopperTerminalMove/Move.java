c0235dbe72787bde0f428cb3cd96903ad7a2c380
dragunwf <plarisanmarc2@gmail.com>
2024-02-27 17:31:53 +0800

    Solve kata https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/java

---
+++ java/8_kyu/GrasshopperTerminalMove/Move.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/java
+
+public class Move {
+    public static int move(int position, int roll) {
+        return position + roll * 2;
+    }
+}
