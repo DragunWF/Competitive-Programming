b726da2b755b0394af6ad40e8bd961cb9ec48014
Marc Plarisan <plarisanmarc2@gmail.com>
2024-03-01 16:32:24 +0800

    Solve kata https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce/train/java

--- java/8_kyu/MultiplicationTable/Kata.java
+++ java/8_kyu/MultiplicationTable/Kata.java
@@ -3,8 +3,10 @@
 class Kata {
     public static String multiTable(int num) {
         StringBuilder output = new StringBuilder();
-        for (int i = 1; i <= 10; i++)
-            output.append(String.format("%s * %s = %s\n", i, num, i * num));
+        for (int i = 1; i <= 10; i++) {
+            output.append(String.format("%s * %s = %s%s",
+                    i, num, i * num, i == 10 ? "" : "\n"));
+        }
         return output.toString();
     }
 }
