5eb26d453b8859a15a2f5bbe1fd596fa2f5c8355
dragunwf <plarisanmarc2@gmail.com>
2024-03-21 21:13:31 +0800

    Solve kata https://www.codewars.com/kata/52f787eb172a8b4ae1000a34/train/java

--- java/Draft/NumberOfTrailingZeroN/Solution.java
+++ java/Draft/NumberOfTrailingZeroN/Solution.java
@@ -4,6 +4,14 @@ import java.math.BigInteger;
 
 public class Solution {
     public static int zeros(int n) {
+        int trailingZeros = n / 5;
+        for (int i = 2, j = (int) Math.pow(5, i); j < n; j = (int) Math.pow(5, ++i)) {
+            trailingZeros += n / j;
+        }
+        return trailingZeros;
+    }
+
+    public static int old(int n) {
         BigInteger factorial = BigInteger.valueOf(1);
         for (int i = 1; i <= n; i++)
             factorial = factorial.multiply(BigInteger.valueOf(i));
@@ -31,5 +39,6 @@ public class Solution {
         System.out.println(zeros(6));
         System.out.println(zeros(12));
         System.out.println(zeros(18));
+        System.out.println(zeros(101));
     }
 }
