c922e6c3444c54ce51d68de34aa6c3b7eeccbc9f
dragunwf <plarisanmarc2@gmail.com>
2024-05-30 20:26:00 +0800

    Solve kata https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/java

---
+++ java/8_kyu/Cockroach/Cockroach.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/java
+
+public class Cockroach {
+    public int cockroachSpeed(double x) {
+        return (int) Math.floor(x * 100000 / 3600);
+    }
+}
