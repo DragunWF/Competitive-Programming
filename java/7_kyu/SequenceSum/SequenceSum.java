df83659e32c94400404dabcc0b1b7eb4d2baab6f
dragunwf <plarisanmarc2@gmail.com>
2024-06-04 19:43:00 +0800

    Solve kata https://www.codewars.com/kata/56e9e4f516bcaa8d4f001763/train/java

--- java/7_kyu/SequenceSum/SequenceSum.java
+++ java/7_kyu/SequenceSum/SequenceSum.java
@@ -2,6 +2,9 @@
 
 public class SequenceSum {
     public static String showSequence(int value) {
+        if (value <= 0) {
+            return value < 0 ? value + "<0" : "0=0";
+        }
         StringBuilder output = new StringBuilder();
         int sum = 0;
         for (int i = 0; i <= value; i++) {
