37ddccd95a074b0ff2fd4d8c7fde7583054fc8aa
dragunwf <plarisanmarc2@gmail.com>
2024-06-05 21:22:05 +0800

    Solve kata https://www.codewars.com/kata/5b853229cfde412a470000d0/train/java

---
+++ java/8_kyu/TwiceAsOld/TwiceAsOld.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/java
+
+public class TwiceAsOld {
+    public static int TwiceAsOld(int dadYears, int sonYears) {
+        return Math.abs(dadYears - sonYears * 2);
+    }
+}
