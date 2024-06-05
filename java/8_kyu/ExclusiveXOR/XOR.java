7a917c9e990b54c0e6190482733c15cf23c0d09a
dragunwf <plarisanmarc2@gmail.com>
2024-06-05 21:03:42 +0800

    Solve kata https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c/solutions/java

---
+++ java/8_kyu/ExclusiveXOR/XOR.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c/solutions/java
+
+public class XOR {
+    public static boolean xor(boolean a, boolean b) {
+        return a ^ b;
+    }
+}
