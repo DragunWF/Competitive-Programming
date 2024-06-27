3491e1959a7a893e6150973939ff8029a07fa045
DragunDev <plarisanmarc@gmail.com>
2024-06-27 19:44:53 +0800

    Solve kata https://www.codewars.com/kata/52adc142b2651f25a8000643/train/java

---
+++ java/8_kyu/SleighAuthentication/Sleigh.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/52adc142b2651f25a8000643/train/java
+
+public class Sleigh {
+    public static Boolean authenticate(String name, String password) {
+        return name.equals("Santa Claus") && password.equals("Ho Ho Ho!");
+    }
+}
