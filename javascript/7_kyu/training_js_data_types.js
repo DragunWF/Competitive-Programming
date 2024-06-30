ce94ea47b4b48b4dbc3051a4ab5a50be041e4293
DragunDev <plarisanmarc@gmail.com>
2024-06-30 14:50:47 +0800

    Solve kata https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript

---
+++ javascript/7_kyu/training_js_data_types.js
@@ -0,0 +1,39 @@
+// https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
+
+let v1 = 50,
+  v2 = 100,
+  v3 = 150,
+  v4 = 200,
+  v5 = 2,
+  v6 = 250;
+
+function equal1() {
+  let a = v1,
+    b = v1;
+  return a + b;
+}
+
+//Please refer to the example above to complete the following functions
+function equal2() {
+  let a = v4, //set number value to a
+    b = v2; //set number value to b
+  return a - b;
+}
+
+function equal3() {
+  let a = v1, //set number value to a
+    b = v5; //set number value to b
+  return a * b;
+}
+
+function equal4() {
+  let a = v4, //set number value to a
+    b = v5; //set number value to b
+  return a / b;
+}
+
+function equal5() {
+  let a = v2, //set number value to a
+    b = v4; //set number value to b
+  return a % b;
+}
