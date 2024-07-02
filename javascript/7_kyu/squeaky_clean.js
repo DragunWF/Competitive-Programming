60a4e685db92534f30b19ddbadb885a50fe1a353
DragunDev <plarisanmarc@gmail.com>
2024-07-02 13:48:10 +0800

    Add comment for slight improvement on previously solved kata https://www.codewars.com/kata/53a8beaca9198e77b9000309/train/javascript

--- javascript/7_kyu/squeaky_clean.js
+++ javascript/7_kyu/squeaky_clean.js
@@ -1,5 +1,7 @@
 // https://www.codewars.com/kata/53a8beaca9198e77b9000309/train/javascript
 
+// arr.filter(Boolean) would have been faster
+
 function squeakyClean(arr) {
   return arr.filter((item) => Boolean(item));
 }
