71edbfa27b32529ce68db6ca1d03d248d12e37a1
DragunDev <plarisanmarc@gmail.com>
2024-07-10 14:38:47 +0800

    Draft kata

---
+++ python/Draft/if_you_can_read_this.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/586538146b56991861000293/train/python
+
+# Do note that this preloaded library is specific to the kata
+
+from preloaded import NATO # NATO['A'] == 'Alfa', etc
+
+def to_nato(words : str) -> str:
+    return ''
