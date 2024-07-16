bb91d1ae83fc1fc5dcebf1387a2d339f4c26a82f
DragunDev <plarisanmarc@gmail.com>
2024-07-16 22:16:33 +0800

    Solve kata https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/java

---
+++ java/8_kyu/KataExampleTwist/KataExampleTwist.java
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/java
+
+public class KataExampleTwist {
+    public static String[] kataExampleTwist() {
+        String[] websites = new String[1000];
+        for (int i = 0; i < 1000; i++) {
+            websites[i] = "codewars";
+        }
+        return websites;
+    }
+}
