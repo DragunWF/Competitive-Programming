085df0b77cedefb47b0e6559da2d3b5a839556d6
DragunDev <plarisanmarc@gmail.com>
2024-07-17 16:45:25 +0800

    Solve kata https://www.codewars.com/kata/57036f007fd72e3b77000023/train/java

---
+++ java/8_kyu/ClassicHelloWorld/Solution.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57036f007fd72e3b77000023/train/java
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
