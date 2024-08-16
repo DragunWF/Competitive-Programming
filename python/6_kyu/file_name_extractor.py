1e1740350270d82c03c338486d2edef38c8286db
DragunDev <plarisanmarc@gmail.com>
2024-08-16 19:36:52 +0800

    Solve kata https://www.codewars.com/kata/597770e98b4b340e5b000071/train/python

---
+++ python/6_kyu/file_name_extractor.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/597770e98b4b340e5b000071/train/python
+
+class FileNameExtractor:
+    @staticmethod
+    def extract_file_name(dirty_file_name):
+        without_first = "_".join(dirty_file_name.split("_")[1:])
+        return ".".join(without_first.split(".")[:-1])
+
+
+print(["a", "b", "c"][:-1])
