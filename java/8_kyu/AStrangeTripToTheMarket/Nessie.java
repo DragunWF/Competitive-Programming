f830bd53a36c73af2551b657d47ef5bd95cf99b0
Marc Plarisan <plarisanmarc2@gmail.com>
2024-08-19 08:17:57 +0800

    Solve kata https://www.codewars.com/kata/55ccdf1512938ce3ac000056/train/java

---
+++ java/8_kyu/AStrangeTripToTheMarket/Nessie.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55ccdf1512938ce3ac000056/train/java
+
+public class Nessie {
+    public static boolean isLockNessMonster(String s) {
+        return s.contains("3.50") || s.contains("tree fiddy") || s.contains("three fifty");
+    }
+}
