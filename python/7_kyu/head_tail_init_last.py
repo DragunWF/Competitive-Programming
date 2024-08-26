c9c4967eeceeeaf80ba1f0f0275a2ea4875f8feb
DragunDev <plarisanmarc@gmail.com>
2024-08-26 17:10:49 +0800

    Solve kata https://www.codewars.com/kata/54592a5052756d5c5d0009c3/train/python

---
+++ python/7_kyu/head_tail_init_last.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/54592a5052756d5c5d0009c3/train/python
+
+def head(arr: list[int]):
+    return arr[0]
+
+
+def tail(arr: list[int]):
+    return arr[1:]
+
+
+def init(arr: list[int]):
+    return arr[0:-1]
+
+
+def last(arr: list[int]):
+    return arr[-1]
