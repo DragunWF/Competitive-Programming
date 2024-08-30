6bf8ce0da7b0be7ed8cb1d4fe7adf466c4afe485
DragunDev <plarisanmarc@gmail.com>
2024-08-30 20:10:05 +0800

    Solve kata https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript

---
+++ javascript/8_kyu/take_an_arrow_to_the_knee.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript
+
+let ArrowFunc = function (arr) {
+  return arr.map((charCode) => String.fromCharCode(charCode)).join("");
+};
