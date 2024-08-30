7629576a77e85ebc0f08277610b121f3fdb7ffe3
DragunDev <plarisanmarc@gmail.com>
2024-08-30 19:58:10 +0800

    Solve kata https://www.codewars.com/kata/5a023c426975981341000014/train/java

---
+++ java/8_kyu/ThirdAngleOfTriangle/ThirdAngle.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5a023c426975981341000014/train/java
+
+public class ThirdAngle {
+    public static int otherAngle(int angle1, int angle2) {
+        return 180 - angle1 - angle2;
+    }
+}
