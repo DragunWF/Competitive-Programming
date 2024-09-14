0d4c7e8f9f45c2c6f170da942fc180c41a90f2c2
DragunDev <plarisanmarc@gmail.com>
2024-09-14 22:50:03 +0800

    Solve kata https://www.codewars.com/kata/55d8618adfda93c89600012e/train/python

---
+++ python/8_kyu/how_do_i_compare_num.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/55d8618adfda93c89600012e/train/python
+
+def what_is(x):
+    if x is 42:
+        return 'everything'
+    if x == 42 * 42:
+        return 'everything squared'
+    return 'nothing'
