8e8460269cdcb3069a77855c268799f367cae5a6
DragunDev <plarisanmarc@gmail.com>
2024-09-24 20:16:30 +0800

    Solve kata https://www.codewars.com/kata/527b3cd0492b6b15250060af/train/java

---
+++ java/8_kyu/PushAHashObjIntoArr/PushAnObjectIntoArray.java
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/527b3cd0492b6b15250060af/train/java
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class PushAnObjectIntoArray {
+    public static List<String> push() {
+        List<String> items = new ArrayList<>();
+        items.add("an object");
+        return items;
+    }
+}
