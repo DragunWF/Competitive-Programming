f1ef4759ed57669f712ce4d0324160487ad095a1
Marc Plarisan <plarisanmarc2@gmail.com>
2024-10-14 07:18:00 +0800

    Solve kata https://www.codewars.com/kata/56f935002e6c0d55fa000d92/train/javascript

---
+++ CodeWars/javascript/7_kyu/fun_with_es6_classes_2.js
@@ -0,0 +1,41 @@
+// https://www.codewars.com/kata/56f935002e6c0d55fa000d92
+
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+
+  introduce() {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
