cd24bd6362cd3e142a8cafdfae1af9dd061b21af
DragunDev <plarisanmarc@gmail.com>
2024-10-18 10:18:40 +0800

    Solve kata https://www.codewars.com/kata/582dafb611d576b745000b74/train/python

---
+++ CodeWars/python/8_kyu/for_ufc_fans.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/582dafb611d576b745000b74
+
+def quote(fighter):
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!" if fighter.lower() == "conor mcgregor" else "I am not impressed by your performance."
