24d671b5d7d2d3c2d1d00e0f4fde85e4717da142
DragunDev <plarisanmarc@gmail.com>
2024-10-18 10:21:13 +0800

    Solve kata https://www.codewars.com/kata/55849d76acd73f6cc4000087/train/python

---
+++ CodeWars/python/8_kyu/online_rpg_player_to_qualifying_stage.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/55849d76acd73f6cc4000087/train/python
+
+def playerRankUp(pts):
+    return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up." if pts >= 100 else False
