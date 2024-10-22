ea1e52ef94ee26d3c7b977a3b651286ec239ca1e
DragunDev <plarisanmarc@gmail.com>
2024-10-22 23:08:21 +0800

    Solve kata https://www.codewars.com/kata/6607fc50c6494c000f1a08fc/train/java

---
+++ CodeWars/java/8_kyu/WeirdJavaArray/Codewarrior.java
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/6607fc50c6494c000f1a08fc/train/java
+
+import java.util.*;
+
+class Codewarrior {
+    public static List<Integer>[] arrayOfLists = new List[] {
+            Arrays.asList(1, 2, 3),
+            Arrays.asList(4, 5, 6),
+            Arrays.asList(7, 8, 9),
+    };
+}
