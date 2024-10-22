a38fe2ff32694796e7168e343e6c377d5ef31caa
DragunDev <plarisanmarc@gmail.com>
2024-10-22 23:00:47 +0800

    Solve kata https://www.codewars.com/kata/670d95c03ad7c741c1099b83/train/csharp

---
+++ CodeWars/csharp/8_kyu/RefactorVariableNameToVariableKeywords.cs
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/670d95c03ad7c741c1099b83/train/csharp
+
+public class VariableName
+{
+    public bool @bool = false;
+    public char @char = '\0';
+    public sbyte @sbyte = 0;
+    public byte @byte = 0;
+    public short @short = 0;
+    public ushort @ushort = 0;
+    public int @int = 0;
+    public uint @uint = 0;
+    public long @long = 0;
+    public ulong @ulong = 0;
+    public float @float = 0;
+    public double @double = 0;
+    public decimal @decimal = 0;
+}
