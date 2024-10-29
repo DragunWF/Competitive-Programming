75b99ad063195595106f40f202419c74ff8178c0
DragunDev <plarisanmarc@gmail.com>
2024-10-29 20:35:41 +0800

    Solve Problem https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true

---
+++ HackerRank/Java/Introduction/WelcomeToJava/Solution.java
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.\nHello, Java.");
+    }
+}
