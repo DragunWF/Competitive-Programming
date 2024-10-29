ff80ee3fb315a66181d1f590cc944c507375ffcc
DragunDev <plarisanmarc@gmail.com>
2024-10-29 19:30:19 +0800

    Solve Problem https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true

---
+++ HackerRank/Python/arithmetic_operators.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(f"{a + b}\n{a - b}\n{a * b}")
