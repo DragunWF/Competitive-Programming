9011d13fd15c891520df0144135d6b09c4de5b18
DragunDev <plarisanmarc@gmail.com>
2024-10-29 19:32:08 +0800

    Solve Problem https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true

---
+++ HackerRank/Python/division.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(f"{a // b}\n{a / b}")
