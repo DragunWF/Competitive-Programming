b1393038c82fae40c9c5329a4e0adba7cc9e56bf
DragunDev <plarisanmarc@gmail.com>
2024-10-29 19:39:33 +0800

    Solve Problem https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true

---
+++ HackerRank/Python/write_a_function.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
+
+def is_leap(year):
+    leap = False
+    
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+    else:
+        leap = False
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
