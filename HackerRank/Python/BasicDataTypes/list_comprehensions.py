b46c8169c2681a1cf62f5f2fff3e6959c23ea6bc
DragunDev <plarisanmarc@gmail.com>
2024-10-29 20:16:40 +0800

    Create list_comprehensions.py

---
+++ HackerRank/Python/BasicDataTypes/list_comprehensions.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+# Hint for myself: Modulo
