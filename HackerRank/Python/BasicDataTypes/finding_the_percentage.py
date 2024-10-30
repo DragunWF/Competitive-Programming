4cb8768b32a8ad17d950f2521cb1ef1729fd221a
DragunDev <plarisanmarc@gmail.com>
2024-10-30 20:41:21 +0800

    Solve Problem https://www.hackerrank.com/challenges/finding-the-percentage/problem

---
+++ HackerRank/Python/BasicDataTypes/finding_the_percentage.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    print("{:.2f}".format(round(sum(student_marks[query_name]) / 3, 2)))
