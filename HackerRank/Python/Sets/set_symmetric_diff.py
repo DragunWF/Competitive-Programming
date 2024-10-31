11d817ccb6526c528ab04baab341b7e308a471a8
DragunDev <plarisanmarc@gmail.com>
2024-10-31 09:48:58 +0800

    Solve Problem https://www.hackerrank.com/challenges/py-set-symmetric-difference-operation/problem?isFullScreen=true

---
+++ HackerRank/Python/Sets/set_symmetric_diff.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/py-set-symmetric-difference-operation/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    input()
+    a = set(input().split(" "))
+    input()
+    b = set(input().split(" "))
+    print(len(a ^ b))
