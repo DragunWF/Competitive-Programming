9a0b54df59940769083be685e7979f871ac1e9c3
DragunDev <plarisanmarc@gmail.com>
2024-11-02 20:42:41 +0800

    Solve Problem https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TAXSAVING

---
+++ CodeChef/Practice/Python/0-500/TAXSAVING.py
@@ -0,0 +1,22 @@
+# https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TAXSAVING
+
+class Solution:
+    @staticmethod
+    def solve():
+        util = Solution
+        t = util.int()
+        for _ in range(t):
+            x, y = util.ints()
+            print(x - y)
+
+    @staticmethod
+    def ints():
+        return map(int, input().split())
+
+    @staticmethod
+    def int():
+        return int(input())
+
+
+if __name__ == "__main__":
+    Solution.solve()
