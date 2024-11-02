a3345255f504e22b2f1e405db499e2a482fd4a5d
DragunDev <plarisanmarc@gmail.com>
2024-11-02 19:47:59 +0800

    Solve Problem https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUCKYSEVEN

---
+++ CodeChef/Practice/Python/0-500/LUCKYSEVEN.py
@@ -0,0 +1,19 @@
+# https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUCKYSEVEN
+
+class Solution:
+    @staticmethod
+    def solve():
+        util = Solution
+        print(input()[6])
+
+    @staticmethod
+    def ints():
+        return map(int, input().split())
+
+    @staticmethod
+    def int():
+        return int(input())
+
+
+if __name__ == "__main__":
+    Solution.solve()
