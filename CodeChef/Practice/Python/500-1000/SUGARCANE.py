c1b8830cf7aa9bf979d328922b0cec5b1fecaaaf
DragunDev <plarisanmarc@gmail.com>
2024-11-02 14:34:17 +0800

    Solve Problem https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUGARCANE

---
+++ CodeChef/Practice/Python/500-1000/SUGARCANE.py
@@ -0,0 +1,7 @@
+# https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUGARCANE
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        n = int(input())
+        print(int(n * 50 * 0.3))
