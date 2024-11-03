2d2e6a4885bc9f56641a291c305ff0543f1c0988
DragunDev <plarisanmarc@gmail.com>
2024-11-03 20:50:37 +0800

    Solve Problem https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT

---
+++ CodeChef/Practice/Python/0-500/BNE_APT.py
@@ -0,0 +1,6 @@
+# https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT
+
+if __name__ == "__main__":
+    n, m = map(int, input().split(" "))
+    x, y = map(int, input().split(" "))
+    print(n * x + m * y)
