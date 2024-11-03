174d6a9d8f2403fd0328689e820aaa0c658b06fb
DragunDev <plarisanmarc@gmail.com>
2024-11-03 20:49:17 +0800

    Solve Problem https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT

---
+++ CodeChef/Practice/Python/0-500/PUZHUNT.py
@@ -0,0 +1,5 @@
+# https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+
+if __name__ == "__main__":
+    n = int(input())
+    print("Yes" if 6 <= n <= 8 else "No")
