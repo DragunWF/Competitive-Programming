f8058a564cebae32051a6a64a623b2ca6eae00e7
Marc Plarisan <plarisanmarc2@gmail.com>
2024-11-04 09:45:36 +0800

    Solve Problem https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FLOW018

---
+++ CodeChef/Practice/Python/500-1000/FLOW018.py
@@ -0,0 +1,9 @@
+# https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FLOW018
+
+from math import factorial
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        print(factorial(n))
