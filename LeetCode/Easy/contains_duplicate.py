5647d8f51715eb2adbc507be133bc37990b21db8
Marc Plarisan <plarisanmarc2@gmail.com>
2024-11-04 12:33:53 +0800

    Refactor solution

--- LeetCode/Easy/contains_duplicate.py
+++ LeetCode/Easy/contains_duplicate.py
@@ -1,11 +1,11 @@
 # https://leetcode.com/problems/contains-duplicate/description/
 
 class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         unique = set()
         for num in nums:
-            if not nums in unique:
-                unique.add(num)
-            else:
-                return False
-        return True
+            if num in unique:
+                return True
+            unique.add(num)
+        return False
+        
