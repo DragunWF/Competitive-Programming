eb73b0a0c385f73918186d2ebc628fa2147dda3b
DragunDev <plarisanmarc@gmail.com>
2024-11-07 17:59:17 +0800

    Solve Problem https://leetcode.com/problems/permutations-ii/description/

---
+++ LeetCode/Medium/permutations_2.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/permutations-ii/description/
+
+from itertools import permutations
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        return list(set(permutations(nums)))
