168ebfd9ffafc353e6a0ad3dac5a93950f283c34
DragunDev <plarisanmarc@gmail.com>
2024-11-10 17:34:19 +0800

    Solve Problem https://leetcode.com/problems/convert-the-temperature/description/

---
+++ LeetCode/Easy/convert_the_temperature.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/convert-the-temperature/description/
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius + 273.15, celsius * 1.8 + 32]
