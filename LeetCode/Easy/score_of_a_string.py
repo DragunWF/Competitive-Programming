57fe17c25cfdeb62ce662747f24b85c3905083f3
DragunDev <plarisanmarc@gmail.com>
2024-11-10 16:51:07 +0800

    Solve Problem https://leetcode.com/problems/score-of-a-string/description/

---
+++ LeetCode/Easy/score_of_a_string.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/score-of-a-string/description/
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        for i in range(len(s) - 1):
+            score += abs(ord(s[i]) - ord(s[i + 1]))
+        return score
