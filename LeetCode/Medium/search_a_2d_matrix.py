75ac11fb02ec45f8e3176964a45b31eae7dd983f
DragunDev <plarisanmarc@gmail.com>
2024-11-12 21:19:42 +0800

    Solve Problem https://leetcode.com/problems/search-a-2d-matrix/description/

---
+++ LeetCode/Medium/search_a_2d_matrix.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/search-a-2d-matrix/description/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            if target in row:
+                return True
+        return False
