06fdd150ef815937b132d8e16798c49f7626f6a8
DragunDev <plarisanmarc@gmail.com>
2024-11-13 17:37:33 +0800

    Fix misplacement of solution

--- LeetCode/Easy/count_the_number_of_consistent_strings.py
+++ LeetCode/Easy/count_the_number_of_consistent_strings.py
@@ -1,5 +1,9 @@
 # https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
 
 class Solution:
-    def sum(self, num1: int, num2: int) -> int:
-        return num1 + num2
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        count = 0
+        for word in words:
+            if all(char in allowed for char in word):
+                count += 1
+        return count
