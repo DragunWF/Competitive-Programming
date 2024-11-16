1cafbe80da3faad59dbb160715324475c76cac5f
DragunDev <plarisanmarc@gmail.com>
2024-11-17 07:46:10 +0800

    Solve Problem https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/

---
+++ LeetCode/Medium/search_in_rotated_array_2.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        return target in nums
