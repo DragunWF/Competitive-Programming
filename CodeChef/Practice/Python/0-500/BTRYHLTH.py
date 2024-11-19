e996ab40437e7f5cb1d7202bc7c5e3adcf3e06e3
DragunDev <plarisanmarc@gmail.com>
2024-11-19 09:59:32 +0800

    Solve Problem https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BTRYHLTH

---
+++ CodeChef/Practice/Python/0-500/BTRYHLTH.py
@@ -0,0 +1,6 @@
+# https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BTRYHLTH
+
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    print("YES" if x >= 80 else "NO")
