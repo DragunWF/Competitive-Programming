8ea783a820668a0ad6c019c3b9b4ae78c16a6ca7
DragunDev <plarisanmarc@gmail.com>
2024-11-19 09:58:34 +0800

    Solve Problem https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD

---
+++ CodeChef/Practice/Python/0-500/CNTWRD.py
@@ -0,0 +1,6 @@
+# https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split(" "))
+    print(n * m)
