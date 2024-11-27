8732954a3ca3da3fb8d37913d0e44e17f104ea2b
DragunDev <plarisanmarc@gmail.com>
2024-11-27 19:45:46 +0800

    Solve Problem https://leetcode.com/problems/find-the-duplicate-number/description/

---
+++ LeetCode/Medium/find_the_duplicate_number.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/find-the-duplicate-number/description/
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
