3e3ef73368ddc650c2ca79634c03ebc331d9bbe9
DragunDev <plarisanmarc@gmail.com>
2024-12-10 13:50:26 +0800

    Solve Problem https://www.codewars.com/kata/58dace86b809ca6c62000ccf/train/javascript

---
+++ CodeWars/javascript/6_kyu/two_numbers_in_an_array_adding_up_to_a_given_number.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/58dace86b809ca6c62000ccf/train/javascript
+
+function hasPair(arr, sum) {
+  arr.sort((a, b) => a - b);
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let currentSum = arr[left] + arr[right];
+    if (currentSum > sum) {
+      right--;
+    } else if (currentSum < sum) {
+      left++;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
