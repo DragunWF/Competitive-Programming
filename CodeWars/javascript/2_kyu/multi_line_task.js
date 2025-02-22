363dbb57455277d8de7635deea7ddcb75f2e6e14
DragunDev <plarisanmarc@gmail.com>
2025-02-22 16:21:50 +0800

    Solve Problem - 2 Kyu Kata https://www.codewars.com/kata/5935558a32fb828aad001213/train/javascript

---
+++ CodeWars/javascript/2_kyu/multi_line_task.js
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/5935558a32fb828aad001213/train/javascript
+
+f=
+''
+[
+'\
+t\
+r\
+i\
+m'
+][
+'\
+b\
+i\
+n\
+d'
+]`
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!`
+
+console.log(f())
+// Equivalent to console.log("".trim.bind("Hello, world!")())
