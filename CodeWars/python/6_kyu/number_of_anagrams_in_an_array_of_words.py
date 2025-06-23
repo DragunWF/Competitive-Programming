4bb09afffb5db06c3eddd807a7649fa39c6d53b8
DragunDev <plarisanmarc@gmail.com>
2025-06-23 12:53:40 +0800

    Solve Problem https://www.codewars.com/kata/587e18b97a25e865530000d8/train/python

---
+++ CodeWars/python/6_kyu/number_of_anagrams_in_an_array_of_words.py
@@ -0,0 +1,26 @@
+# https://www.codewars.com/kata/587e18b97a25e865530000d8/train/python
+
+from collections import Counter
+
+
+def anagram_counter(words: list) -> list:
+    for i in range(len(words)):
+        words[i] = "".join(sorted([*words[i]]))
+
+    print(words)
+    anagram_count = 0
+    word_counter = Counter(words)
+    for key in word_counter:
+        count = word_counter[key]
+        if count >= 2:
+            anagram_count += count * (count - 1) // 2
+    return anagram_count
+
+
+def test() -> None:
+    print(anagram_counter(["dell", "ledl", "abc", "cba"]))
+    print(anagram_counter(["dell", "ledl", "abc", "cba", "bca", "bac"]))
+
+
+if __name__ == "__main__":
+    test()
