76768c6903f847851cb80c1dc0257e5e651639c4
DragunWF <plarisanmarc2@gmail.com>
2025-07-24 11:21:41 +0800

    Solve Problem https://www.codewars.com/kata/5822d89270ca28c85c0000f3/train/python

---
+++ CodeWars/python/7_kyu/string_scramble.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/5822d89270ca28c85c0000f3/train/python
+
+def scramble(s: str, array: list[int]) -> str:
+    output = [*(" " * len(array))]
+    print(output)
+    for i, position in enumerate(array):
+        output[position] = s[i]
+    return "".join(output)
+
+
+def test() -> None:
+    print(scramble("abcd", [0, 3, 1, 2]))  # 'acdb'
+
+
+if __name__ == "__main__":
+    test()
