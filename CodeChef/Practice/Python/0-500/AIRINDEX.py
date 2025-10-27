73d68188456e0016b3bdb94cb56748faa1a4a7ef
DragunWF <plarisanmarc2@gmail.com>
2025-10-27 12:12:31 +0800

    Solve Problem https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRINDEX

---
+++ CodeChef/Practice/Python/0-500/AIRINDEX.py
@@ -0,0 +1,10 @@
+# https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRINDEX
+
+
+def solve() -> None:
+    x = int(input())
+    print("yes" if x < 100 else "no")
+
+
+if __name__ == "__main__":
+    solve()
