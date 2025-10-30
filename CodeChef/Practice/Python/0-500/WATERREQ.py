12da5bdf63f6b494b7b4f4ffc8294b742dfa1d68
DragunWF <plarisanmarc2@gmail.com>
2025-10-30 22:08:28 +0800

    Solve Problem https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ

---
+++ CodeChef/Practice/Python/0-500/WATERREQ.py
@@ -0,0 +1,10 @@
+# https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
+
+def solve() -> None:
+    for _ in range(int(input())):
+        n = int(input())
+        print(2 * n)
+
+
+if __name__ == "__main__":
+    solve()
