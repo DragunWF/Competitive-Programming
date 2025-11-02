3040d35d19bda341457292697aba82b00b59bdb1
DragunWF <plarisanmarc2@gmail.com>
2025-11-02 20:24:16 +0800

    Solve Problem https://www.codewars.com/kata/josephus-survivor

---
+++ CodeWars/python/5_kyu/josephus_survivor.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/josephus-survivor
+
+def josephus_survivor(n: int, k: int) -> int:
+    winner = 0
+    for i in range(2, n + 1):
+        winner = (winner + k) % i
+    return winner + 1
+
+
+def test() -> None:
+    # Expected: 7
+    print(josephus_survivor(7, 3))
+
+
+if __name__ == "__main__":
+    test()
