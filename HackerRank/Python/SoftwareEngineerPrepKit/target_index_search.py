e1e70fa0c6ed023a6a7759453b3872ab705d10ec
DragunWF <plarisanmarc2@gmail.com>
2025-11-16 17:23:38 +0800

    Solve Problem https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/lookup-with-binary-search/problem?isFullScreen=true

---
+++ HackerRank/Python/SoftwareEngineerPrepKit/target_index_search.py
@@ -0,0 +1,48 @@
+# https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/lookup-with-binary-search/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'binarySearch' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY nums
+#  2. INTEGER target
+#
+
+def binarySearch(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
+
+
+if __name__ == '__main__':
+    nums_count = int(input().strip())
+
+    nums = []
+
+    for _ in range(nums_count):
+        nums_item = int(input().strip())
+        nums.append(nums_item)
+
+    target = int(input().strip())
+
+    result = binarySearch(nums, target)
+
+    print(result)
