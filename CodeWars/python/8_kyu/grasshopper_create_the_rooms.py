51532e6d41c483abc3b485ded84851276b3d73a7
DragunWF <plarisanmarc2@gmail.com>
2025-12-29 11:39:28 +0800

    Solve Problem https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/python

---
+++ CodeWars/python/8_kyu/grasshopper_create_the_rooms.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/python
+
+rooms = {
+    "name": {
+        "one": {}, "two": {}, "three": {}
+    },
+    "description": {
+        "one": {}, "two": {}, "three": {}
+    },
+    "completed": {
+        "one": {}, "two": {}, "three": {}
+    }
+}
