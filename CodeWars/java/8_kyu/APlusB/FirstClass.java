064c639484f481249053bb7445b09371048e276d
DragunWF <plarisanmarc2@gmail.com>
2026-04-10 23:56:32 +0800

    Solve Problem - Byte (A + B) in Java https://www.codewars.com/kata/5512a0b0509063e57d0003f5/train/java

---
+++ CodeWars/java/8_kyu/APlusB/FirstClass.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5512a0b0509063e57d0003f5/train/java
+
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        return (byte)(a + b);
+    }
+}
