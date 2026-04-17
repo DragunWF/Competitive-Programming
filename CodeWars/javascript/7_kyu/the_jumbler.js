f357a63374b61677e92e9dc10ff3dfc71fe4c105
DragunWF <plarisanmarc2@gmail.com>
2026-04-17 17:31:31 +0800

    Solve Problem - The Jumbler https://www.codewars.com/kata/69269262ced9e95dc63abd1e

---
+++ CodeWars/javascript/7_kyu/the_jumbler.js
@@ -0,0 +1,23 @@
+//  
+
+function jumbler(indices) {
+  let jumbleCount = 0;
+  let currentValue = indices[0];
+  while (currentValue != 0) {
+    for (let i = 0; i < indices.length; i++) {
+      if (i === currentValue) {
+        const targetValue = indices[i];
+        const newIndices = [];
+        for (let j = 0; j < indices.length; j++) {
+          if (i !== j) {
+            newIndices.push(indices[j]);
+          }
+        }
+        indices = [targetValue, ...newIndices];
+      }
+    }
+    currentValue = indices[0];
+    jumbleCount++;
+  }
+  return jumbleCount;
+}
