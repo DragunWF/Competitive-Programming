9a75dde6e24da04a8a19968185b5a9abdb1bc919
DragunWF <plarisanmarc2@gmail.com>
2026-05-30 20:13:27 +0800

    Solve Problem - Add Two Promises https://leetcode.com/problems/add-two-promises/

---
+++ LeetCode/Easy/JavaScript/add_two_promises.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/add-two-promises/
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+  return (await promise1) + (await promise2);
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
