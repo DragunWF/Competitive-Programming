115ec184c0022e596f8820ebc4633a3cfa0e31a9
DragunWF <plarisanmarc2@gmail.com>
2026-07-01 10:45:53 +0800

    Solve Problem - Interval Cancellation https://leetcode.com/problems/interval-cancellation/

---
+++ LeetCode/Easy/JavaScript/interval_cancellation.js
@@ -0,0 +1,65 @@
+// https://leetcode.com/problems/interval-cancellation/
+
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  fn(...args);
+  const timer = setInterval(() => {
+    fn(...args);
+  }, t);
+  const cancelFn = () => {
+    clearInterval(timer);
+  };
+  return cancelFn;
+};
+
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellableOld = function (fn, args, t) {
+  const output = [];
+  let currentTime = 0;
+  while (true) {
+    // condition to change
+    let returned = fn(...args);
+    output.push({ time: currentTime, returned: 0 });
+    currentTime += t;
+  }
+  return output;
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)
+ */
