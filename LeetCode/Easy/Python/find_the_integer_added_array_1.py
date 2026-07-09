39be2f400ec4ae2bf4b9422c6cf2f0a72552da84
DragunWF <plarisanmarc2@gmail.com>
2026-07-09 21:30:54 +0800

    Solve Problem - Find the Integer Added to Array 1 https://leetcode.com/problems/find-the-integer-added-to-array-i/

---
+++ LeetCode/Easy/Python/find_the_integer_added_array_1.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/find-the-integer-added-to-array-i/
+
+from typing import List
+
+
+class Solution:
+    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+        return min(nums2) - min(nums1)
